b6b3063f9301f332a3cbff58f2fa7520f17fe1c1
peter hewitson <peter.hewitson@reedonline.co.uk>
2021-05-21 15:47:40 +0100

    First test added

---
+++ tdd-katas/FizzBuzz.Library/FizzBuzzService.cs
@@ -0,0 +1,10 @@
+﻿namespace FizzBuzz.Library
+{
+    public class FizzBuzzService
+    {
+        public string GetValueFor(int value)
+        {
+            return "1";
+        }
+    }
+}
---
+++ tdd-katas/FizzBuzz.Tests/FizzBuzzTests.cs
@@ -0,0 +1,20 @@
+﻿using Xunit;
+
+namespace FizzBuzz.Library
+{
+    public class FizzBuzzTests
+    {
+        private readonly FizzBuzzService _sut;
+
+        public FizzBuzzTests()
+        {
+            _sut = new FizzBuzzService();
+        }
+
+        [Fact]
+        public void FizzBuzzValueForOneShouldBeOne()
+        {
+            var result = _sut.GetValueFor(1);
+        }
+    }
+}
